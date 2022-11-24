d8507dfd72cabc61efb15695ba2da15901d308eb
Aniket2021448 <108399480+Aniket2021448@users.noreply.github.com>
2022-11-24 19:20:12 +0530

    Update threadCreation.c

--- Assignment3/threadCreation.c
+++ Assignment3/threadCreation.c
@@ -1,10 +1,68 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<perror.h>
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <stdlib.h>
+
+#define SYS_kern_2D_memcpy 451
 
 int main(){
-    
-    printf("Hello world!");
+    float matrix1[4][3] = {{45.0,8.0,6.0},{4.0,78.0,44.0},{89.0,88.0,72.0},{56.0,71.0,36.0}};
+    float matrix2[4][3] = {{7.0,7.0,7.0},{7.0,7.0,7.0},{7.0,7.0,7.0},{7.0,7.0,7.0}};
 
+    printf("FIRST ORIGINAL MATRIX\n");
+    printf("==========================================\n");
+
+    for (int row1 = 0; row1 < 4; row1++)
+    {
+        for (int column1 = 0; column1 < 3; column1++)
+        {
+            printf("%f ", matrix1[row1][column1]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    printf("SECOND ORIGINAL MATRIX\n");
+    printf("========================================\n");
+
+    for (int row2 = 0; row2 < 4; row2++)
+    {
+        for (int column2 = 0; column2 < 3; column2++)
+        {
+            printf("%f ", matrix2[row2][column2]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    int usingSysCall = syscall(SYS_kern_2D_memcpy, matrix1, matrix2, 4,3);
+    printf("RESULTING FIRST ORIGINAL MATRIX AFTER MAKING A SYSCALL\n");
+    printf("=========================================\n");
+
+    for (int frow1 = 0; frow1 < 4; frow1++)
+    {
+        for (int fcolumn1 = 0; fcolumn1 < 3; fcolumn1++)
+        {
+            printf("%f ", matrix1[frow1][fcolumn1]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    printf("RESULTING SECOND ORIGINAL MATRIX AFTER MAKING A SYSCALL\n");
+    printf("=================================\n");
+
+
+    for (int frow2 = 0; frow2 < 4; frow2++)
+    {
+        for (int fcolumn2 = 0; fcolumn2 < 3; fcolumn2++)
+        {
+            printf("%f ", matrix2[frow2][fcolumn2]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return 0;
 }
